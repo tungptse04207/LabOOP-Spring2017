618465b98dd5feed785ffef9e36ba80693c70496
tungptse04207 <tungptse04207@fpt.edu.vn>
2017-01-14 17:04:30 +0700

    Create Main.java

---
+++ J1.L.P0023/vn/edu/fpt/P00023/gui/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vn.edu.fpt.P00023.gui;
+
+import vn.edu.fpt.P00023.entity.Fruit;
+import vn.edu.fpt.P00023.entity.Order;
+import java.util.ArrayList;
+import java.util.List;
+import vn.edu.fpt.P00023.bo.BusinessObject;
+
+/**
+ *
+ * @author crazyboy
+ */
+public class Main {
+    public static void main(String[] args) {
+        BusinessObject p=new BusinessObject();
+        List<Fruit> dbFruit=new ArrayList<>();
+        List<Order> dbOrder = new ArrayList<>();
+        while(true){
+            p.menu(dbFruit,dbOrder);
+        }
+    }
+}
